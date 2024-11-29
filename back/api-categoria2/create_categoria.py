fe733874632714f4e76510aaacfa6d430c02984a
Dannam21 <danna.mendez@utec.edu.pe>
2024-11-29 09:20:29 -0500

    Cambios en categoria

--- back/api-categoria2/create_categoria.py
+++ back/api-categoria2/create_categoria.py
@@ -10,15 +10,17 @@ table = dynamodb.Table(table_name)
 def lambda_handler(event, context):
     body = event['body']
     tenant_id = body['tenant_id']
-    nombre = body['nombre']
     categoria_id = str(uuid.uuid4())
+    nombre = body['nombre']
     fecha_creacion = datetime.datetime.utcnow().isoformat()
+    data = body.get('data')
 
     categoria = {
         'tenantID': tenant_id,
         'categoriaID': categoria_id,
         'nombre': nombre,
-        'fechaCreacion': fecha_creacion
+        'fechaCreacion': fecha_creacion,
+        'data': data
     }
 
     response = table.put_item(Item=categoria)
