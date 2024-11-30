c53af8d6b0e5ff464b82123fc9b56202517c4d5c
Dannam21 <danna.mendez@utec.edu.pe>
2024-11-30 16:53:04 -0500

    categoria create

--- back/api-categoria2/delete_categoria.py
+++ back/api-categoria2/delete_categoria.py
@@ -16,6 +16,9 @@ def lambda_handler(event, context):
     categoria_id = body['categoria_id']
     tenant_id = body['tenant_id']
 
+    # Agregar depuración para ver los valores recibidos
+    print(f"Received categoria_id: {categoria_id}, tenant_id: {tenant_id}")
+
     try:
         # Eliminar el item usando categoria_id y tenant_id como claves
         response = table.delete_item(
@@ -27,6 +30,7 @@ def lambda_handler(event, context):
 
         # Comprobación de la respuesta para asegurar que el ítem se eliminó
         if 'Attributes' not in response:
+            print(f"Item with categoria_id: {categoria_id} and tenant_id: {tenant_id} not found.")
             return {
                 'statusCode': 404,
                 'body': json.dumps('Categoría no encontrada')
@@ -38,6 +42,7 @@ def lambda_handler(event, context):
         }
 
     except Exception as e:
+        print(f"Error al eliminar la categoría: {str(e)}")
         return {
             'statusCode': 500,
             'body': json.dumps(f'Error al eliminar la categoría: {str(e)}')
