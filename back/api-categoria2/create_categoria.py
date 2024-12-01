b5d192893d8a80b1e78437b7d85dbfa948235da7
Dannam21 <danna.mendez@utec.edu.pe>
2024-12-01 11:28:18 -0500

    categoria create

--- back/api-categoria2/create_categoria.py
+++ back/api-categoria2/create_categoria.py
@@ -15,38 +15,6 @@ def lambda_handler(event, context):
     # Verificar si 'body' es una cadena JSON, y si es así, convertirla a diccionario
     if isinstance(body, str):
         body = json.loads(body)  # Deserializar el string JSON en un diccionario
-
-    token = event['headers'].get('Authorization')
-    if not token:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Authorization token is missing'})
-        }
-
-        # Crear el cliente de Lambda para invocar la validación del token
-    lambda_client = boto3.client('lambda')
-
-        # Construir la carga útil para validar el token
-    payload_string = json.dumps({
-        "token": token,
-        "tenant_id": tenant_id,
-        "user_id": user_id
-    })
-
-        # Invocar la función de validación del token
-    invoke_response = lambda_client.invoke(
-        FunctionName="ValidarTokenAcceso",  # Asegúrate de que el nombre de la función sea correcto
-        InvocationType='RequestResponse',
-        Payload=payload_string
-    )
-    
-        # Leer la respuesta de la validación del token
-    response1 = json.loads(invoke_response['Payload'].read().decode())
-    if response1['statusCode'] == 403:
-        return {
-            'statusCode': 403,
-            'body': json.dumps({'error': 'Forbidden - Acceso No Autorizado'})
-        }
     
     # Extraer los datos del cuerpo deserializado
     tenant_id = body['tenant_id']
--- back/api-producto2/delete_producto.py
+++ back/api-producto2/delete_producto.py
@@ -1,46 +1,20 @@
 import boto3
-import logging
 import os
-import json
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 table_name = os.environ['TABLE_NAME']
 table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
-    try:
-        # Parsear el cuerpo de la solicitud
-        body = json.loads(event['body'])
-        tenant_id = body.get('tenant_id')
-        producto_id = body.get('producto_id')
+    body = event['body']
+    tenant_id = body['tenant_id']
+    producto_id = body['producto_id']
 
-        if not tenant_id or not producto_id:
-            return {
-                'statusCode': 400,
-                'body': json.dumps({'error': 'Debe proporcionar tenant_id y producto_id'})
-            }
+    response = table.delete_item(
+        Key={'tenantID': tenant_id, 'productoID': producto_id}
+    )
 
-        delete_response = table.delete_item(
-            Key={'tenant_id': tenant_id, 'producto_id': producto_id}
-        )
-
-        if 'Attributes' not in delete_response:
-            return {
-                'statusCode': 404,
-                'body': json.dumps({'error': 'Producto no encontrado'})
-            }
-
-        return {
-            'statusCode': 204,
-            'body': json.dumps({'message': 'Producto eliminado exitosamente'})
-        }
-
-    except Exception as e:
-        logger.error("Error al eliminar el producto: %s", str(e))
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': f'Error interno: {str(e)}'})
-        }
+    return {
+        'statusCode': 200,
+        'body': 'Producto eliminado'
+    }
