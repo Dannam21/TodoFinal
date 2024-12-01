6c43c9f882aa42008a481e2a0ff0d886ec4fcbcc
jdluques <juan.luque@utec.edu.pe>
2024-12-01 14:01:02 -0500

    Create pedidos

--- back/api-pedido2/create_pedidos.js
+++ back/api-pedido2/create_pedidos.js
@@ -2,16 +2,32 @@ const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    const { email, nombre, password } = JSON.parse(event.body);
+    const { tenant_id, usuario_id, datos } = JSON.parse(event.body);
+    const producto_id = crypto.randomUUID();
+    const estado = "PENDIENTE";
+
+    if (!tenant_id || !usuario_id || !datos) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Missing required data (tenant_id, usuario_id, datos)'
+            })
+        };
+    }
+
     const params = {
-        TableName: "Pedidos",
-        Item: { email, nombre, password, createdAt: new Date().toISOString() },
+        TableName: process.env.PEDIDOS_TABLE,
+        Item: { tenant_id, producto_id, usuario_id,
+                estado,
+                datos,
+                createdAt: new Date().toISOString() },
     };
 
     try {
         await dynamoDB.put(params).promise();
         return { statusCode: 201, body: JSON.stringify({ message: "Pedido creado" }) };
     } catch (error) {
+        console.error("Error creating order:", error);
         return { statusCode: 500, body: JSON.stringify({ message: "Error al crear pedido", error }) };
     }
 };
