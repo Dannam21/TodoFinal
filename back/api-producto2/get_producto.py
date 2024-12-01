b2eb0f2be9a34335fb106f3f94a9cb5ef5e8c057
jdluques <juan.luque@utec.edu.pe>
2024-12-01 13:11:01 -0500

    Create product decimal fix

--- back/api-producto2/get_producto.py
+++ back/api-producto2/get_producto.py
@@ -2,23 +2,38 @@ import boto3
 import os
 import logging
 import json
+from decimal import Decimal
 
 # Configuración de logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
+# Función para convertir objetos Decimal a tipos JSON serializables
+def decimal_to_str(obj):
+    """Convierte recursivamente cualquier objeto Decimal a su valor serializable JSON."""
+    if isinstance(obj, Decimal):
+        return float(obj) if obj % 1 else int(obj)  # Convierte Decimal a float o int
+    elif isinstance(obj, dict):
+        return {k: decimal_to_str(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [decimal_to_str(i) for i in obj]
+    else:
+        return obj
+
+
 def lambda_handler(event, context):
     try:
         # Obtén el nombre de la tabla desde las variables de entorno
-        table_name = os.environ.get('TABLE_NAME', 'DefaultTable') 
+        table_name = os.environ.get('TABLE_NAME', 'DefaultTable')
         logger.info(f"Nombre de la tabla: {table_name}")
-        
+
         # Accede a los parámetros queryStringParameters
         tenant_id = event['queryStringParameters'].get('tenant_id', None)
         categoria_nombre = event['queryStringParameters'].get('categoria_nombre', None)
         producto_id = event['queryStringParameters'].get('producto_id', None)
         stock = event['queryStringParameters'].get('stock', None)
-        
+
         # Verificar si todos los parámetros requeridos están presentes
         if not tenant_id or not categoria_nombre or not producto_id:
             logger.error("Faltan parámetros requeridos: tenant_id, categoria_nombre o producto_id")
@@ -27,11 +42,12 @@ def lambda_handler(event, context):
                 'body': json.dumps({'message': 'tenant_id, categoria_nombre y producto_id son obligatorios'})
             }
 
-        logger.info(f"Parámetros recibidos - tenant_id: {tenant_id}, categoria_nombre: {categoria_nombre}, producto_id: {producto_id}")
-        
+        logger.info(
+            f"Parámetros recibidos - tenant_id: {tenant_id}, categoria_nombre: {categoria_nombre}, producto_id: {producto_id}")
+
         # Crear el recurso DynamoDB
         dynamodb = boto3.resource('dynamodb')
-        
+
         # Obtener la tabla de DynamoDB usando el nombre de la tabla
         table = dynamodb.Table(table_name)
 
@@ -52,11 +68,15 @@ def lambda_handler(event, context):
                 break
 
         if not item:
-            logger.info(f"Producto no encontrado para tenant_id: {tenant_id}, categoria_nombre: {categoria_nombre} y producto_id: {producto_id}")
+            logger.info(
+                f"Producto no encontrado para tenant_id: {tenant_id}, categoria_nombre: {categoria_nombre} y producto_id: {producto_id}")
             return {
                 'statusCode': 404,
                 'body': json.dumps({'message': 'Producto no encontrado'})
             }
+
+        # Convertir los valores de Decimal a tipos JSON serializables antes de devolver la respuesta
+        item = decimal_to_str(item)
 
         logger.info(f"Producto encontrado: {item}")
 
@@ -65,7 +85,7 @@ def lambda_handler(event, context):
             'statusCode': 200,
             'body': json.dumps({'producto': item})  # Convertimos el item a JSON
         }
-    
+
     except Exception as e:
         logger.error(f"Error inesperado: {str(e)}")
         return {
