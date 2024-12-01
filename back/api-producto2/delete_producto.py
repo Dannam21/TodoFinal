1939495f1608a603a48c775c454d444f86f83c28
jdluques <juan.luque@utec.edu.pe>
2024-12-01 15:38:05 -0500

    Update pedidos

--- back/api-producto2/delete_producto.py
+++ back/api-producto2/delete_producto.py
@@ -1,8 +1,6 @@
 import boto3
 import os
 import json
-from decimal import Decimal
-
 
 # Inicializar recursos DynamoDB
 dynamodb = boto3.resource('dynamodb')
@@ -14,11 +12,41 @@ def lambda_handler(event, context):
         # Decodificar el cuerpo de la solicitud
         body = json.loads(event['body'])
 
-        # Extraer parámetros
+        # Obtener el token de autorización
+        token = event['headers'].get('Authorization')
+        if not token:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Authorization token is missing'})
+            }
+
+        # Validar el token y verificar el rol
+        lambda_client = boto3.client('lambda')
+        payload = {
+            "token": token,
+            "role": "admin"
+        }
+
+        # Invocar la función de validación de token
+        invoke_response = lambda_client.invoke(
+            FunctionName="api-usuarios-dev-ValidarTokenAcceso",  # Cambia al nombre correcto de tu función
+            InvocationType='RequestResponse',
+            Payload=json.dumps(payload)
+        )
+
+        # Leer la respuesta de la validación del token
+        response1 = json.loads(invoke_response['Payload'].read().decode())
+        if response1['statusCode'] != 200:
+            return {
+                'statusCode': 403,
+                'body': json.dumps({'error': 'Forbidden - Solo los administradores pueden eliminar productos'})
+            }
+
+        # Extraer parámetros del cuerpo
         tenant_id = body.get('tenant_id')
         producto_id = body.get('producto_id')
 
-        # Validar parámetros
+        # Validar parámetros obligatorios
         if not tenant_id or not producto_id:
             return {
                 'statusCode': 400,
