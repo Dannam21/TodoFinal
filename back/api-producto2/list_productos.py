457dd8af782a5ea052520069d0b84cf163f2e956
Dannam21 <danna.mendez@utec.edu.pe>
2024-12-01 11:35:25 -0500

    categoria create

--- back/api-producto2/list_productos.py
+++ back/api-producto2/list_productos.py
@@ -1,25 +1,25 @@
-import boto3
-import os
+# import boto3
+# import os
 
-dynamodb = boto3.resource('dynamodb')
-table_name = os.environ['TABLE_NAME']
-table = dynamodb.Table(table_name)
+# dynamodb = boto3.resource('dynamodb')
+# table_name = os.environ['TABLE_NAME']
+# table = dynamodb.Table(table_name)
 
 
-def lambda_handler(event, context):
-    tenant_id = event['queryStringParameters']['tenantID']
-    limit = int(event['queryStringParameters'].get('limit', 10))
+# def lambda_handler(event, context):
+#     tenant_id = event['queryStringParameters']['tenantID']
+#     limit = int(event['queryStringParameters'].get('limit', 10))
 
-    response = table.query(
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('tenantID').eq(tenant_id),
-        Limit=limit
-    )
+#     response = table.query(
+#         KeyConditionExpression=boto3.dynamodb.conditions.Key('tenantID').eq(tenant_id),
+#         Limit=limit
+#     )
 
-    items = response.get('Items', [])
+#     items = response.get('Items', [])
 
-    return {
-        'statusCode': 200,
-        'body': {
-            'productos': items
-        }
-    }
+#     return {
+#         'statusCode': 200,
+#         'body': {
+#             'productos': items
+#         }
+#     }
