47306d532d7cc63855c147b7b3962639c0298e9d
jdluques <juan.luque@utec.edu.pe>
2024-12-01 14:38:49 -0500

    Updated serverless & Delete - Pedidos

--- back/api-pedido2/delete_pedidos.js
+++ back/api-pedido2/delete_pedidos.js
@@ -2,15 +2,20 @@ const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    const { email } = JSON.parse(event.body);
+    const { tenant_id, producto_id, pedido_id } = JSON.parse(event.body);
+    const tenantID_productoID = tenant_id + "#" + producto_id;
+
     const params = {
-        TableName: "Pedidos",
-        Key: { email },
+        TableName: process.env.PEDIDOS_TABLE,
+        Key: {
+            'tenant_id#producto_id': tenantID_productoID,
+            'pedido_id': pedido_id
+        },
     };
 
     try {
         await dynamoDB.delete(params).promise();
-        return { statusCode: 200, body: JSON.stringify({ message: "Pedido eliminado" }) };
+        return { statusCode: 204, body: JSON.stringify({ message: "Pedido eliminado" }) };
     } catch (error) {
         return { statusCode: 500, body: JSON.stringify({ message: "Error al eliminar pedido", error }) };
     }
